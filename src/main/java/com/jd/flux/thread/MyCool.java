7c989f48cf0523e043b923434173bc9695980ce3
wangyingjie1 <shab2@Hello7>
2017-12-21 18:48:04 +0800

    add  jar

---
+++ src/main/java/com/jd/flux/thread/MyCool.java
@@ -0,0 +1,15 @@
+package com.jd.flux.thread;
+
+/**
+ * @author: wangyingjie1
+ * @version: 1.0
+ * @createdate: ${date} ${time}
+ */
+public class MyCool {
+
+    public static void main(String[] args) {
+
+        System.out.println(MyCallable.class.isAssignableFrom(MyCool.class));
+
+    }
+}
